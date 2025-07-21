118e06d789a434d54e9c1bc99bd8ffd3f270e057
felipogit <felipeesped0023@gmail.com>
2025-07-21 10:30:32 -0300

    Rota de listar autores

--- Services/Autor/AutorService.cs
+++ Services/Autor/AutorService.cs
@@ -1,6 +1,45 @@
-﻿namespace WebApi.Services.Autor
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+using WebApi.Models;
+
+namespace WebApi.Services.Autor
 {
     public class AutorService : IAutorInterface
     {
+        private readonly AppDbContext _context;
+        public AutorService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Task<ResponseModel<AutorModel>> BuscarAutorPorId(int idAutor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public  Task<ResponseModel<AutorModel>> BuscarAutotLivro(int idLivro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ResponseModel<List<AutorModel>>> ListarAutores()
+        {
+            ResponseModel<List<AutorModel>> resposta = new ResponseModel<List<AutorModel>>();
+            try 
+            { 
+                var autores = await _context.Autores.ToListAsync();
+                resposta.Dados = autores;
+                resposta.Mensagem = "Lista de autores obtida com sucesso.";
+
+                return resposta;
+
+            }
+            catch(Exception ex)
+            {
+             
+                resposta.Mensagem = ex.Message;
+                resposta.Status = false;
+                return resposta;
+            }
+        }
     }
 }
--- Services/Autor/IAutorInterface.cs
+++ Services/Autor/IAutorInterface.cs
@@ -1,6 +1,11 @@
-﻿namespace WebApi.Services.Autor
+﻿using WebApi.Models;
+
+namespace WebApi.Services.Autor
 {
     public interface IAutorInterface
     {
+        Task<ResponseModel<List<AutorModel>>> ListarAutores();
+        Task<ResponseModel<AutorModel>> BuscarAutorPorId(int idAutor);
+        Task<ResponseModel<AutorModel>> BuscarAutotLivro(int idLivro);
     }
 }
